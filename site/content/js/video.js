0a1e1e53a7698de7d4b6087fa54a25be4f9b8d40
mariiuus <mariiuus@hotmail.com>
2015-11-09 17:29:23 +0100

    leker meg med css
    fjernet css'n sebastian hadde lagt til fordi det fucket opp for meg. han
    må bruke id

--- site/content/js/video.js
+++ site/content/js/video.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function () {
     "use strict";
     if (window.innerWidth < 1300) {
+        console.log(window.innerWidth)
         document.getElementById("videoWrapper").removeChild(document.getElementById("mainVideo"));
     }
 });
