db1e0b5111f262122605f7bb60a21a69c305f41b
Andreas Dørum <andreas.doerum@gmail.com>
2015-11-09 18:54:53 +0100

    add main tag. Update js to work with new structure.

--- site/content/js/video.js
+++ site/content/js/video.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
     "use strict";
     if (window.innerWidth < 1300) {
-        document.getElementById("videoWrapper").removeChild(document.getElementById("mainVideo"));
+        document.getElementById("front").removeChild(document.getElementById("mainVideo"));
     }
 });
