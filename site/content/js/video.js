3a6a2fd92c33990a0837426d2414966473a5a81b
mariiuus <mariiuus@hotmail.com>
2015-11-09 14:01:07 +0100

    javascript for video
    siden bakgrunnsbildet er bak videoen kan en like godt fjerne videoen om
    skjermen er for liten, og ha standard bakgrunnsbilde istedenfor

--- site/content/js/video.js
+++ site/content/js/video.js
@@ -1,8 +1,6 @@
 window.addEventListener("load", function () {
     "use strict";
-    if (window.innerWidth > 1300) {
-        console.log("hei")
-        document.getElementById("mainVideo").setAttribute('autoplay', '');
-        document.getElementById("mainVideo").setAttribute("loop", "");
+    if (window.innerWidth < 1300) {
+        document.getElementById("videoWrapper").removeChild(document.getElementById("mainVideo"));
     }
 });
