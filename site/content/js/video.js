f61ae3e3f97ac489732347f29a44b4dc40cb783a
mariiuus <mariiuus@hotmail.com>
2015-11-10 13:35:49 +0100

    video.js var av en eller annen grunn blitt fjernet...

---
+++ site/content/js/video.js
@@ -0,0 +1,1 @@
+window.addEventListener("load", function () {     "use strict";     if (window.innerWidth < 1300) {         document.getElementById("front").removeChild(document.getElementById("mainVideo"));     } });
