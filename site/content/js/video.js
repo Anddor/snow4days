5e1c94126f2c0d98dd64aa19950abf17f3005bf2
Thorben Werner Sjøstrøm Dahl <twdahl@stud.ntnu.no>
2015-11-16 11:52:16 +0100

    Synchronized org-server with git. Information about authors added. Image moved into img-folder. Removed images that are unused. Fixed styling of about and contact us. Other minor changes.

--- site/content/js/video.js
+++ site/content/js/video.js
@@ -1,1 +1,7 @@
+/*
+    FILE NAME: video.js
+    WRITTEN BY:  Marius Aaesnes
+    WHEN:  October 2015
+    PURPOSE:  If the user is using a screen that is smaller than 1300 (this is just a value i chose, and can be changed) the video is removed, so that it doesn't play on phones, and the background picture is showing instead (it is behind the video)
+*/
 window.addEventListener("load", function () {     "use strict";     if (window.innerWidth < 1300) {         document.getElementById("front").removeChild(document.getElementById("mainVideo"));     } });
